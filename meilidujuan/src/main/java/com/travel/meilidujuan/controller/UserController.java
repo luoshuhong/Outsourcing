bd3072e13251f0f54ff3c2bf7ffe2e54bf4082be
luoshuhong <luoshuhong.jian@foxmail.com>
2015-08-27 23:19:52 +0800

    修改符合restful 风格接口

--- meilidujuan/src/main/java/com/travel/meilidujuan/controller/UserController.java
+++ meilidujuan/src/main/java/com/travel/meilidujuan/controller/UserController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSONObject;
@@ -13,42 +14,46 @@ import com.alibaba.fastjson.JSONObject;
 @RequestMapping("/user")
 public class UserController {
 	
-	@RequestMapping("/add")
+	@RequestMapping(method=RequestMethod.POST)
+//	@RequestMapping("/add")
     @ResponseBody
     public String add(HttpServletRequest request, HttpServletResponse response){
 		JSONObject job = new JSONObject();
 		String data = request.getParameter("data");
-        job.put("result", "success");
+        job.put("result", "success---add");
         System.out.println("---add");
         return job.toJSONString();
     }
 	
-	@RequestMapping("/del")
+	@RequestMapping(method=RequestMethod.DELETE)
+//	@RequestMapping("/del")
     @ResponseBody
     public String del(HttpServletRequest request, HttpServletResponse response){
 		JSONObject job = new JSONObject();
 		String data = request.getParameter("data");
-        job.put("result", "success");
+        job.put("result", "success---del");
         System.out.println("---del");
         return job.toJSONString();
     }
 	
-	@RequestMapping("/update")
+	@RequestMapping(method=RequestMethod.PATCH)
+//	@RequestMapping("/update")
     @ResponseBody
     public String update(HttpServletRequest request, HttpServletResponse response){
 		JSONObject job = new JSONObject();
 		String data = request.getParameter("data");
-        job.put("result", "success");
+        job.put("result", "success---update");
         System.out.println("---update");
         return job.toJSONString();
     }
 	
-	@RequestMapping("/query")
+	@RequestMapping(method=RequestMethod.GET)
+//	@RequestMapping("/query")
     @ResponseBody
     public String query(HttpServletRequest request, HttpServletResponse response){
 		JSONObject job = new JSONObject();
 		String data = request.getParameter("data");
-        job.put("result", "success");
+        job.put("result", "success---query");
         System.out.println("---query");
         return job.toJSONString();
     }
