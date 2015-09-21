b0a5b3bef064a9a60acadbee9aa8df142856f05b
刘锦涛 <liujintao@joyschool.cn>
2015-09-21 18:08:11 +0800

    前端独立出去

--- meilidujuan-fe/src/actions/activities-action.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by liunickluck on 15/9/5.
- */
-var Reflux=require("reflux");
-var ActivityActions = Reflux.createActions([
-    'getActivities'
-]);
-module.exports = ActivityActions;
--- meilidujuan-fe/src/templates/MailTpl.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by liunickluck on 15/9/7.
- */
-var template = __inline("./MailTemplate.tpl");
-module.exports = template;
